8cffa52bb1a54dd9bf6fce9f3f60f821d7f4a43d
valmgoff <valmgoff@gmail.com>
2026-02-21 18:34:13 -0700

    simplified flip behavior
    also tweaked movement

--- DAGV1700/AdventureGame/Assets/Tools/Transform/FlipTransformBehaviour.cs
+++ DAGV1700/AdventureGame/Assets/Tools/Transform/FlipTransformBehaviour.cs
@@ -5,13 +5,15 @@
 /// It listens for two specific key presses to determine the direction of the flip.
 /// </summary>
 [Tooltip("Controls the flipping of the orientation of a GameObject based on user key presses.")]
+[RequireComponent(typeof(SpriteRenderer))]
 public class FlipTransformBehaviour : MonoBehaviour
 {
-    [Tooltip("The rotation in degrees to apply when the first key is pressed.")]
-    public float directionRight = 0;
+    SpriteRenderer render;
 
-    [Tooltip("The rotation in degrees to apply when the second key is pressed.")]
-    public float directionLeft = 180;
+    private void Start()
+    {
+        render = this.GetComponent<SpriteRenderer>();
+    }
 
     /// <summary>
     /// Flip the GameObject rotation based on key input, every frame.
@@ -20,16 +22,14 @@ public class FlipTransformBehaviour : MonoBehaviour
     {
         float moveInput = Input.GetAxis("Horizontal"); // left or right float [-1-1]
 
-        // If the first key is pressed -> rotate the GameObject
-        if (moveInput < 0f)
+        if (moveInput < 0f) // left
         {
-            transform.rotation = Quaternion.Euler(0, directionRight, 0);
+            render.flipX = true;
         }
 
-        // If the second key is pressed -> rotate the GameObject
-        if (0f < moveInput)
+        if (0f < moveInput) // right
         {
-            transform.rotation = Quaternion.Euler(0, directionRight, 0);
+            render.flipX = false;
         }
     }
 }
