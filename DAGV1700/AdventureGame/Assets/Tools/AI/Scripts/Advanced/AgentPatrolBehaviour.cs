80edbba5851f514648614a791e9c2226d55a8559
valmgoff <valmgoff@gmail.com>
2026-04-02 13:15:55 -0600

    added pause to enemy

--- DAGV1700/AdventureGame/Assets/Tools/AI/Scripts/Advanced/AgentPatrolBehaviour.cs
+++ DAGV1700/AdventureGame/Assets/Tools/AI/Scripts/Advanced/AgentPatrolBehaviour.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,40 +7,76 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent), typeof(SpriteRenderer))]
 public class AgentPatrolBehaviour : MonoBehaviour
 {
-    // original variables
+    // editor dials
+    [SerializeField]
+    private float remainingDistanceNum = 0.5f;
+    [SerializeField]
+    private List<Transform> patrolPointList;
+    [SerializeField]
+    private float pauseForSec = 2;
+    // pointers
     private NavMeshAgent agent;
-    public float remainingDistanceNum = 0.5f;
-    public List<Transform> patrolPointList;
-    private int i;
+    private Coroutine waitCoroutine;
+    // vars
+    private int nextIndex;
+    private bool isIdle;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        ChangeTarget(); // begin walk
     }
 
     void Update()
     {
         // checking pathing
-        if (agent.pathPending || !(agent.remainingDistance < remainingDistanceNum)) // check if we should change target
+        if (agent.pathPending || agent.remainingDistance > remainingDistanceNum) // check if we should change target
         {
             return;
         }
+        // begin swapping behavior
+        if (waitCoroutine == null) // create only one Coroutine
+        {
+            waitCoroutine = StartCoroutine(WaitEnum());
+        }
+    }
 
-        Transform nextPoint = patrolPointList[i];
+    // coroutine function
+    IEnumerator WaitEnum()
+    {
+        isIdle = true;
+        yield return new WaitForSeconds(pauseForSec);
+        ChangeTarget();
+        CleanCoroutine();
+    }
+
+    private void ChangeTarget()
+    {
+        Transform nextPoint = patrolPointList[nextIndex];
+
         // flipping
-        float distanceToNext = nextPoint.position.x - this.transform.position.x;
-        Boolean goingLeft = distanceToNext < 0; // target is left (negative x) from curr pos
         SpriteRenderer sprite = this.GetComponent<SpriteRenderer>();
+        float distanceToNext = nextPoint.position.x - this.transform.position.x;
+        bool goingLeft = distanceToNext < 0; // target is left (negative x) from curr pos
         if (goingLeft)
-        {
             sprite.flipX = true;
-        }
         else
-        {
             sprite.flipX = false;
-        }
+
         // navigation
         agent.destination = nextPoint.position; // align to next target
-        i = (i + 1) % patrolPointList.Count; // increment tracker to next-point
+        nextIndex = (nextIndex + 1) % patrolPointList.Count; // itterate through points
+        isIdle = false;
+    }
+
+    private void CleanCoroutine()
+    {
+        StopAllCoroutines();
+        waitCoroutine = null;
+    }
+
+    public bool getIsIdle()
+    {
+        return isIdle;
     }
 }
