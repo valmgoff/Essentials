11f7a65f85086d34462f4956abaab7fe90b9f40b
valmgoff <valmgoff@gmail.com>
2026-05-08 10:30:28 -0600

    fixed text display, teaking enemies

--- DAGV1700/AdventureGame/Assets/Scripts/ItemTracker.cs
+++ DAGV1700/AdventureGame/Assets/Scripts/ItemTracker.cs
@@ -1,39 +1,53 @@
 ﻿using UnityEngine;
 using TMPro;
 using System;
+using Unity.VisualScripting;
 
 [RequireComponent(typeof(TextMeshProUGUI))]
 public class ItemTracker : MonoBehaviour
 {
     [SerializeField] private InventorySO observedInventory;
     [SerializeField] private bool showZero;
+    [SerializeField] private Id targetItem;
 
     private TextMeshProUGUI display;
 
     private void Start()
     {
-        WaitForSeconds waitForFixedUpdate = new WaitForSeconds(0.1f); // Set delay time for UpdateNumberCount
         display = GetComponent<TextMeshProUGUI>();
-        UpdateText(""); // blank until update externally
+        UpdateCounter();
     }
 
-    public void UpdateText(string newText)
+    public void ChangeTargetItem(Id newItem)
     {
+        targetItem = newItem;
+        UpdateCounter();
+    }
+
+    public void UpdateCounter() // updating functionality
+    {
+        int count = observedInventory.Talley(targetItem);
+        if (count == 0 && showZero == false)
+            return; // no change to display
+
+        String newText = count.ToString() + " " + targetItem.PluralName();
+        UpdateText(newText);
+    }
+
+    private void UpdateText(string newText) // text processing
+    {
+        // error check
         if (newText == null)
         {
-            Debug.LogError("Updating text with null string.", this);
+            Debug.LogError("Tried to update text with null string.", this);
+            // no change to display
             return;
         }
-        display.text = newText;
-    }
-
-    public void UpdateCounter(Id item)
-    {
-        int count = observedInventory.Talley(item);
-        if (showZero || count != 0)
+        // updates text
+        if (newText.Equals(""))
         {
-            String newText = count.ToString() + " " + item.PluralName();
-            UpdateText(newText);
+            Debug.Log("String updated blank!");
         }
+        display.SetText(newText);
     }
 }
